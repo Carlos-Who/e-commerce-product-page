52b71e36ae1c2626a204f895b8a8392dd2b881f2
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-12 12:28:08 -0600

    feat(cart): Add the cart dialog animation 💫
    Animate the cart dialog states, enter and leave.

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {MatBadge} from "@angular/material/badge";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'element-cart',
@@ -8,6 +9,15 @@ import {MatBadge} from "@angular/material/badge";
   imports: [
     NgOptimizedImage,
     MatBadge
+  ],
+  animations: [
+    trigger('cartDialogAnimationTrigger', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateY(-10px)'}),
+        animate('100ms', style({opacity: 1, transform: 'translateY(0)'}))
+      ]),
+      transition(':leave', [animate('100ms', style({opacity: 0, transform: 'translateY(-10px)'}))]),
+    ]),
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
