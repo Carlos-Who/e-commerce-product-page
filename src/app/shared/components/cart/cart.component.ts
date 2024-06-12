2c307921008f0407f37e197180cd8c4f0b6ae5fc
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-12 12:21:25 -0600

    feat(cart): Add the basic cart dialog structure & styles 🚧
    Add the HTML structure for the cart dialog component.
    Add the corresponding styles.
    Add the toggle logic for the cart dialog.

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {MatBadge} from "@angular/material/badge";
 
 @Component({
   selector: 'element-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    MatBadge
+  ],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+
+  isCartDialogOpen: boolean = false;
+
+
+  public toggleCartDialog(): void {
+    this.isCartDialogOpen = !this.isCartDialogOpen;
+  }
 
 }
