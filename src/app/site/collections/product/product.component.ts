d1ca314658dd500ca5b43750d0dab7bffa34dfaa
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-12 18:10:15 -0600

    feat(product-page): Add the basic structure & styles of the product page 🚧

--- src/app/site/collections/product/product.component.ts
+++ src/app/site/collections/product/product.component.ts
@@ -1,13 +1,44 @@
-import {Component} from '@angular/core';
-
+import {Component, inject, effect} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgClass, NgOptimizedImage} from "@angular/common";
+import {BreakpointsService} from "../../../core/services/breakpoints/breakpoints.service";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FormsModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+
+  public selectedQuantity: number = 0;
+
+  public breakpointsService: BreakpointsService  = inject(BreakpointsService);
+  public currentBreakpoint: string = "";
+
+
+  constructor() {
+    effect(() => {
+      this.currentBreakpoint = this.breakpointsService.currentScreenSize();
+    });
+
+  }
+
+  increment(): void {
+
+  }
+
+  decrement(): void {
+
+  }
+
+  addToCart(): void {
+
+  }
 
 }
