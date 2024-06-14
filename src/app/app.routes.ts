d4e6ba702777d78a94d6ff0956265add827fc65d
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-14 07:31:54 -0600

    feat(routes): Add default dummy route 🚀

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ProductComponent} from "./site/collections/product/product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "collections/products/fall-limited-edition-sneakers",
+    component: ProductComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/collections/products/fall-limited-edition-sneakers",
+  }
+];
