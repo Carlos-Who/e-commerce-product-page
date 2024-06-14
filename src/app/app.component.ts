4503c8196f86d594bce07bdbda35ded2df371b37
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-14 07:30:38 -0600

    feat(app): Add implementation of app component 🚀
    This will add the following:
    - Integrate the main layout components
    - Implement the breakpoint observer
    - Add the app container style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./shared/layout/header/header.component";
+import {FooterComponent} from "./shared/layout/footer/footer.component";
+import {HeaderReductedComponent} from "./shared/layout/header-reducted/header-reducted.component";
+import {BreakpointsService} from "./core/services/breakpoints/breakpoints.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeaderReductedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'e-commerce-product-page';
+
+  public breakpointsService: BreakpointsService  = inject(BreakpointsService);
+  public currentBreakpoint: string = "";
+
+  constructor() {
+    effect(() => {
+      this.currentBreakpoint = this.breakpointsService.currentScreenSize();
+    });
+  }
+
 }
