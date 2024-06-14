650c3ae332e42d9b8417de346b5fbc0bab1b1e09
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-14 07:23:16 -0600

    feat(lightbox): Add empty lightbox component ✨

---
+++ src/app/shared/components/lightbox/lightbox.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-lightbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './lightbox.component.html',
+  styleUrl: './lightbox.component.scss'
+})
+export class LightboxComponent {
+
+}
