c7ce7a003354d7604ed0bdc8063dfd0b43b226a6
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-06-14 04:55:39 -0600

    feat(header-reducted): Add header-reducted basic structure 🚧

--- src/app/shared/layout/header-reducted/header-reducted.component.ts
+++ src/app/shared/layout/header-reducted/header-reducted.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {CartComponent} from "../../components/cart/cart.component";
+import {MatTooltip} from "@angular/material/tooltip";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header-reducted',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    CartComponent,
+    MatTooltip,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './header-reducted.component.html',
   styleUrl: './header-reducted.component.scss'
 })
 export class HeaderReductedComponent {
+
+  public isMobileMenuOpen: boolean = false;
+
+  public toggleMobileMenu(): void {
+
+  }
 
 }
